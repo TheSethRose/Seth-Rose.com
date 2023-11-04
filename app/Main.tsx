297eb29018835cf205d8d08d7fa9c1a91c173b86
Seth Rose <admin@seth-rose.com>
2023-11-04 18:07:30 -0500

    Attempting to resolve deployment issues

--- app/Main.tsx
+++ app/Main.tsx
@@ -2,19 +2,27 @@ import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
-import NewsletterForm from 'pliny/ui/NewsletterForm'
 import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
-import type { Authors } from 'contentlayer/generated'
-
-interface Props {
-  posts: any
-  content: Omit<Authors, '_id' | '_raw' | 'body'>
-}
+import { useEffect, useState } from 'react'
+import { Authors, allAuthors } from 'contentlayer/generated'
+import { coreContent } from 'pliny/utils/contentlayer'
 
 const MAX_DISPLAY = 3
 
-export default function Home({ posts, content }: Props) {
+export default function Home({ posts }) {
+  const [content, setContent] = useState(null)
+
+  useEffect(() => {
+    const author = allAuthors.find((p) => p.slug === 'default') as Authors
+    const mainContent = coreContent(author)
+    setContent(mainContent)
+  }, [])
+
+  if (!content) {
+    return null // or some loading indicator
+  }
+
   const {
     name,
     avatar,
@@ -71,7 +79,7 @@ export default function Home({ posts, content }: Props) {
       <div className='divide-y divide-gray-200 dark:divide-gray-700'>
         <div className='space-y-2 pb-8 pt-6 md:space-y-5'>
           <h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'>
-            Recent Posts
+            Latest Posts
           </h1>
           <p className='text-lg leading-7 text-gray-500 dark:text-gray-400'>
             {siteMetadata.description}
@@ -83,7 +91,49 @@ export default function Home({ posts, content }: Props) {
             const { slug, date, title, summary, tags } = post
             return (
               <li key={slug} className='py-12'>
-                <article>{/* ... rest of your post item JSX ... */}</article>
+                <article>
+                  <div className='space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0'>
+                    <dl>
+                      <dt className='sr-only'>Published on</dt>
+                      <dd className='text-base font-medium leading-6 text-gray-500 dark:text-gray-400'>
+                        <time dateTime={date}>
+                          {formatDate(date, siteMetadata.locale)}
+                        </time>
+                      </dd>
+                    </dl>
+                    <div className='space-y-5 xl:col-span-3'>
+                      <div className='space-y-6'>
+                        <div>
+                          <h2 className='text-2xl font-bold leading-8 tracking-tight'>
+                            <Link
+                              href={`/blog/${slug}`}
+                              className='text-gray-900 dark:text-gray-100'
+                            >
+                              {title}
+                            </Link>
+                          </h2>
+                          <div className='flex flex-wrap'>
+                            {tags.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
+                          </div>
+                        </div>
+                        <div className='prose max-w-none text-gray-500 dark:text-gray-400'>
+                          {summary}
+                        </div>
+                      </div>
+                      <div className='text-base font-medium leading-6'>
+                        <Link
+                          href={`/blog/${slug}`}
+                          className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'
+                          aria-label={`Read more: "${title}"`}
+                        >
+                          Read more &rarr;
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </article>
               </li>
             )
           })}
