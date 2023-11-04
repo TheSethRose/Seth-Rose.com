e6c2c4e2a7bafa16763e7c16acadcace9ee71b20
Seth Rose <admin@seth-rose.com>
2023-11-04 17:55:31 -0500

    Attempting to fix deployment again

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,21 +3,18 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
-
-// Imported from Author Page
-import { ReactNode } from 'react'
-import type { Authors } from 'contentlayer/generated'
 import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
+import type { Authors } from 'contentlayer/generated'
 
 interface Props {
-  children: ReactNode
+  posts: any
   content: Omit<Authors, '_id' | '_raw' | 'body'>
 }
 
 const MAX_DISPLAY = 3
 
-export default function Home({ posts }) {
+export default function Home({ posts, content }: Props) {
   const {
     name,
     avatar,
@@ -31,10 +28,11 @@ export default function Home({ posts }) {
 
   return (
     <>
+      {/* About Me Section */}
       <div className='divide-y divide-gray-200 dark:divide-gray-700'>
         <div className='space-y-2 pb-8 pt-6 md:space-y-5'>
           <h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'>
-            About Seth
+            About
           </h1>
         </div>
         <div className='items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0'>
@@ -61,10 +59,15 @@ export default function Home({ posts }) {
             </div>
           </div>
           <div className='prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2'>
-            Test
+            Hello World
           </div>
         </div>
       </div>
+
+      {/* Separator */}
+      <div className='my-8 border-t border-gray-200 dark:border-gray-700'></div>
+
+      {/* Latest Posts Section */}
       <div className='divide-y divide-gray-200 dark:divide-gray-700'>
         <div className='space-y-2 pb-8 pt-6 md:space-y-5'>
           <h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'>
@@ -80,49 +83,7 @@ export default function Home({ posts }) {
             const { slug, date, title, summary, tags } = post
             return (
               <li key={slug} className='py-12'>
-                <article>
-                  <div className='space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0'>
-                    <dl>
-                      <dt className='sr-only'>Published on</dt>
-                      <dd className='text-base font-medium leading-6 text-gray-500 dark:text-gray-400'>
-                        <time dateTime={date}>
-                          {formatDate(date, siteMetadata.locale)}
-                        </time>
-                      </dd>
-                    </dl>
-                    <div className='space-y-5 xl:col-span-3'>
-                      <div className='space-y-6'>
-                        <div>
-                          <h2 className='text-2xl font-bold leading-8 tracking-tight'>
-                            <Link
-                              href={`/blog/${slug}`}
-                              className='text-gray-900 dark:text-gray-100'
-                            >
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className='flex flex-wrap'>
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div>
-                        </div>
-                        <div className='prose max-w-none text-gray-500 dark:text-gray-400'>
-                          {summary}
-                        </div>
-                      </div>
-                      <div className='text-base font-medium leading-6'>
-                        <Link
-                          href={`/blog/${slug}`}
-                          className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'
-                          aria-label={`Read more: "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </article>
+                <article>{/* ... rest of your post item JSX ... */}</article>
               </li>
             )
           })}
