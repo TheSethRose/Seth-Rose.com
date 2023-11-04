c56f3e427cc52a8c644a9510fe0e9be5b42acfb3
Seth Rose <admin@seth-rose.com>
2023-11-04 17:19:46 -0500

    Update to Homepage & Navigation

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,9 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
+  { href: '/blog/hello-world', title: 'About Me' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/blog/hello-world', title: 'About Me' },
+  { href: '/projects', title: 'Projects' },  
 ]
 
 export default headerNavLinks
