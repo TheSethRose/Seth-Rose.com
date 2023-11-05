8373e4b4bbc7b475b8aedbd9211f8dddfebb2693
Seth Rose <admin@seth-rose.com>
2023-11-04 19:39:08 -0500

    Working on spacing and padding

--- app/Main.tsx
+++ app/Main.tsx
@@ -9,12 +9,6 @@ import { coreContent } from 'pliny/utils/contentlayer'
 
 const MAX_DISPLAY = 3
 
-/**
- * Renders the home page.
- *
- * @param {Object} posts - an array of posts
- * @return {JSX.Element} the rendered home page
- */
 export default function Home({ posts }) {
   const author = allAuthors.find((p) => p.slug === 'default') as Authors
   const content = coreContent(author)
@@ -34,13 +28,13 @@ export default function Home({ posts }) {
     <>
       {/* About Me Section */}
       <div className='divide-y divide-gray-200 dark:divide-gray-700 my-1'>
-        <div className='space-y-2 pb-8 pt-6 md:space-y-5 max-w-6xl mx-auto'>
+        <div className='space-y-2 pb-4 pt-3 md:space-y-5 max-w-6xl mx-auto'>
           <h3 className='text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14'>
             Hi there! I'm Seth!
           </h3>
         </div>
-        <div className='items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0 mt-1 mb-1 max-w-6xl mx-auto'>
-          <div className='flex flex-col items-center space-x-2 pt-8'>
+        <div className='items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0 mt-4 mb-4 max-w-6xl mx-auto'>
+          <div className='flex flex-col items-center space-x-2 pt-4'>
             {avatar && (
               <Image
                 src={avatar}
@@ -50,19 +44,19 @@ export default function Home({ posts }) {
                 className='h-36 w-36 rounded-full'
               />
             )}
-            <h3 className='pb-2 pt-4 text-xl font-bold leading-8 tracking-tight'>
+            <h3 className='pt-4 text-xl font-bold leading-8 tracking-tight'>
               {name}
             </h3>
             <div className='text-gray-500 dark:text-gray-400'>{occupation}</div>
             <div className='text-gray-500 dark:text-gray-400'>{company}</div>
-            <div className='flex space-x-3 pt-6'>
+            <div className='flex space-x-3 pt-4'>
               <SocialIcon kind='mail' href={`mailto:${email}`} />
               <SocialIcon kind='github' href={github} />
               <SocialIcon kind='linkedin' href={linkedin} />
               <SocialIcon kind='twitter' href={twitter} />
             </div>
           </div>
-          <div className='prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2'>
+          <div className='prose max-w-none pt-4 pb-4 dark:prose-invert xl:col-span-2'>
             <p>
               I'm a Senior Software Engineer specializing in the ServiceNow
               platform. I’m passionate about creating streamlined, automated
@@ -85,7 +79,7 @@ export default function Home({ posts }) {
 
       {/* Latest Posts Section */}
       <div className='divide-y divide-gray-200 dark:divide-gray-700 my-1'>
-        <div className='space-y-2 pb-8 pt-6 md:space-y-5 max-w-6xl mx-auto'>
+        <div className='space-y-2 pb-4 pt-3 md:space-y-5 max-w-6xl mx-auto'>
           <h3 className='text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14'>
             Latest Posts
           </h3>
@@ -95,21 +89,19 @@ export default function Home({ posts }) {
           {posts.slice(0, MAX_DISPLAY).map((post) => {
             const { slug, date, title, summary, tags } = post
             return (
-              <li key={slug} className='py-12'>
+              <li key={slug} className='py-8'>
                 <article>
                   <div className='space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0'>
                     <dl>
                       <dt className='sr-only'>Published on</dt>
                       <dd className='text-base font-medium leading-6 text-gray-500 dark:text-gray-400'>
-                        <time dateTime={date}>
-                          {formatDate(date, siteMetadata.locale)}
-                        </time>
+                        <time dateTime={date}>{formatDate(date)}</time>
                       </dd>
                     </dl>
                     <div className='space-y-5 xl:col-span-3'>
                       <div className='space-y-6'>
                         <div>
-                          <h3 className='text-xl font-bold leading-8 tracking-tight'>
+                          <h3 className='text-2xl font-bold leading-8 tracking-tight'>
                             <Link
                               href={`/blog/${slug}`}
                               className='text-gray-900 dark:text-gray-100'
@@ -123,17 +115,17 @@ export default function Home({ posts }) {
                             ))}
                           </div>
                         </div>
-                        <div className='prose text-gray-500 max-w-none dark:text-gray-400'>
+                        <div className='prose max-w-none text-gray-500 dark:text-gray-400'>
                           {summary}
                         </div>
                       </div>
                       <div className='text-base font-medium leading-6'>
                         <Link
                           href={`/blog/${slug}`}
-                          className='text-blue-500 hover:text-blue-600 dark:hover:text-blue-400'
+                          className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'
                           aria-label={`Read "${title}"`}
                         >
-                          Read more →
+                          Read more &rarr;
                         </Link>
                       </div>
                     </div>
@@ -144,17 +136,6 @@ export default function Home({ posts }) {
           })}
         </ul>
       </div>
-      {posts.length > MAX_DISPLAY && (
-        <div className='flex justify-end text-base font-medium leading-6'>
-          <Link
-            href='/blog'
-            className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'
-            aria-label='All posts'
-          >
-            All Posts &rarr;
-          </Link>
-        </div>
-      )}
     </>
   )
 }
