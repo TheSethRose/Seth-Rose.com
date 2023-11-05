9b8838a534adfe50f5ff47f880cc09e203381854
Seth Rose <admin@seth-rose.com>
2023-11-05 00:43:41 -0500

    Update X Icon

--- components/social-icons/icons.tsx
+++ components/social-icons/icons.tsx
@@ -35,11 +35,10 @@ export function Mail(svgProps: SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
-
 export function Twitter(svgProps: SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' {...svgProps}>
-      <path d='M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z' />
+      <path d='M467.04 57.6h84.72L366.72 269.04 584.4 556.8H414L280.44 382.32 127.8 556.8H42.96l197.88-226.2L32.16 57.6h174.72l120.6 159.48L467.04 57.6zm-29.76 448.56h46.92L181.32 105.6h-50.4l306.36 400.56z' />
     </svg>
   )
 }
